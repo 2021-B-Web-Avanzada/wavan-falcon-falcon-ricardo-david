1daa97c45c3935d8017e42c5f63477712f621199
Ricardo <ricardo.falcon@epn.edu.ec>
2021-12-07 08:54:26 -0500

    create/02-typescript/01-variables.js y 02-interfaces.js

---
+++ 02-typescript/01-variables.js
@@ -0,0 +1,23 @@
+var nombre = 'Ricardo'; //primitivo
+var nombre2 = 'Ricardo2'; //clase String
+// nombre = 1;
+nombre = 'David';
+var edad = 24;
+var casado = false;
+var fecha = new Date();
+var sueldo;
+sueldo = 12.4;
+//Duck Typing
+var apellido = 'Falcon'; //string
+apellido = 'Ricardo'; //igual a otros string
+apellido.toUpperCase(); //metodos string
+var marihuana = 2;
+marihuana = '2';
+marihuana = true;
+marihuana = function () { return '2'; };
+var edadMultiple = 2;
+edadMultiple = '2';
+edadMultiple = 222;
+edadMultiple = 'dos';
+edadMultiple = new Date();
+//edadMultiple = true;
